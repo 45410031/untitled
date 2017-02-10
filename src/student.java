ef7054a0e0ed9cdff5617f27b5056e4160a2a5d9
45410031 <275826362@qq.com>
2017-02-10 16:43:49 +0800

    Initial commit

---
+++ src/student.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Administrator on 2017/2/10.
+ */
+public class student {
+
+    private int id;
+    private int age;
+    private String name;
+
+    public student() {
+    }
+
+    public student(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
